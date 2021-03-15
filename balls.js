bd0658bd67c2f38f394f710fec5b6f9b1739391a
AnoushkaPal924 <64146264+AnoushkaPal924@users.noreply.github.com>
2021-03-15 08:13:15 +0530

    Add files via upload

--- balls.js
+++ balls.js
@@ -1,24 +1,34 @@
-class Ball{
-    constructor(x, y, radius,options) {
-        var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0,
-            isStatic : true
-        }
-        this.body = Matter.Bodies.circle(x, y, radius, options);
-        //this.body = ellipse(x, y, radius, options);
-          this.radius = radius
+class Ball
+{
+    constructor(x,y,r)
+    {
+        var options={
+            isStatic:false,
+            restitution:1,
+            friction:0,
+            density:0.8
+            
+            }
+        this.x=x;
+        this.y=y;
+        this.r=r
+        
+        this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
         World.add(world, this.body);
-      }
-      display(){
-        push();
-        //translate(this.body.position.x, this.body.position.y);
-        ellipseMode(CENTER);
-        ellipse( this.body.position.x, this.body.position.y, this.radius,this.radius);
-        fill ("#D79158")
-        pop();
-      }
-
-     
+    }
+    display()
+    {
+            
+            var paperpos=this.body.position;
+            push()
+            translate(paperpos.x, paperpos.y);
+            rectMode(CENTER)
+            //strokeWeight(4);
+            fill('#75876E')
+            //imageMode(CENTER);
+            //image(this.image, 0,0,this.r, this.r)
+            ellipse(0,0,this.r, this.r);
+            pop()
+            
+    }
 }
--- string.js
+++ string.js
@@ -1,49 +1,27 @@
 class Sling{
-    constructor(bodyA, bodyB, offsetX, offsetY){
-
-        this.offsetX = offsetX;
-        this.offsetY = offsetY;
-
-        var options = {
-            body1: bodyA,
-            body2: bodyB,
-            pointB:{x:this.offsetX,y:this.offsetY},
-            stiffness: 0.04,
-            length: 10
+    constructor(body1,body2, offsetX, offsetY)
+    {
+        this.offsetX=offsetX
+        this.offsetY=offsetY
+        var options={
+            bodyA:body1,
+            bodyB:body2,
+            pointB:{x:this.offsetX, y:this.offsetY}
         }
-        //this.sling1 = loadImage('sprites/sling1.png');
-        //this.sling2 = loadImage('sprites/sling2.png');
-        //this.sling3 = loadImage('sprites/sling3.png');
-        this.bodyB = bodyB;
-        this.bodyA = bodyA;
-       
-        this.sling = Constraint.create(options);
-        World.add(world, this.sling);
+        //console.log(options);
+        this.rope=Constraint.create(options)
+        World.add(world,this.rope)
     }
-    attach(body){
-        this.sling.bodyA = body;
-    }
-    
-    fly(){
-        this.sling.bodyA = null;
-    }
-
-    display(){
-        
-        var pointA = this.sling.pointA.position;
-        var pointB = this.sling.pointB.position;
-
-        strokeWeight(2);
-
-        var Anchor1X = pointA.x
-        var Anchor1Y = pointA.y
-
-        var Anchor2X = pointB.x + this.offsetX;
-        var Anchor2Y = pointB.x + this.offsetX;
-
+    display()
+    {
+        var pointA=this.rope.bodyA.position;
+        var pointB=this.rope.bodyB.position;
+        //strokeWeight();
+        fill ('#D79158');
+        var Anchor1X=pointA.x
+        var Anchor1Y=pointA.y
+        var Anchor2X=pointB.x+this.offsetX
+        var Anchor2Y=pointB.y+this.offsetY
         line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
-            
-            pop();
-        }
     }
-    
+}
